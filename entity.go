6aa83bdc525c94917eee7b9a98c0cc42c454f062
Alex Pliutau <a.pliutau@gmail.com>
2018-08-21 15:26:53 +0700

    Add CreateEntity

--- entity.go
+++ entity.go
@@ -1,9 +1,25 @@
 package witai
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 )
+
+// NewEntity - https://wit.ai/docs/http/20170307#post__entities_link
+type NewEntity struct {
+	ID  string `json:"id"`
+	Doc string `json:"doc"`
+}
+
+// Entity - https://wit.ai/docs/http/20170307#post__entities_link
+type Entity struct {
+	ID      string `json:"id"`
+	Doc     string `json:"doc"`
+	Name    string `json:"name"`
+	Lang    string `json:"lang"`
+	BuiltIn bool   `json:"builtin"`
+}
 
 // GetEntities - returns list of entities. https://wit.ai/docs/http/20170307#get__entities_link
 func (c *Client) GetEntities() ([]string, error) {
@@ -17,9 +33,29 @@ func (c *Client) GetEntities() ([]string, error) {
 	var entities []string
 	decoder := json.NewDecoder(resp)
 	err = decoder.Decode(&entities)
+	return entities, err
+}
+
+// CreateEntity - Creates a new entity with the given attributes. https://wit.ai/docs/http/20170307#post__entities_link
+func (c *Client) CreateEntity(e NewEntity) (*Entity, error) {
+	entityJSON, err := json.Marshal(e)
 	if err != nil {
-		return []string{}, err
+		return nil, err
 	}
 
-	return entities, nil
+	resp, err := c.request(http.MethodPost, "/entities", "application/json", bytes.NewBuffer(entityJSON))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Close()
+
+	var entity *Entity
+	decoder := json.NewDecoder(resp)
+	err = decoder.Decode(&entity)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity, nil
 }
--- entity_test.go
+++ entity_test.go
@@ -21,3 +21,22 @@ func TestGetEntities(t *testing.T) {
 		t.Fatalf("expected 2 entities, got: %d", len(entities))
 	}
 }
+
+func TestCreateEntity(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte(`{"lang": "en"}`))
+	}))
+	defer func() { testServer.Close() }()
+
+	apiBase = testServer.URL
+
+	c := NewClient("token")
+	e, err := c.CreateEntity(NewEntity{
+		ID:  "favorite_city",
+		Doc: "A city that I like",
+	})
+
+	if err != nil || e.Lang != "en" {
+		t.Fatalf("lang=en expected, got: %s", e.Lang)
+	}
+}

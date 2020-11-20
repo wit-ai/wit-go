34c9e8cf775387782859aea12f0649a4f923bcf8
Nicky Logan <laurentiusdominick@yahoo.com>
2020-11-21 00:43:23 +0700

    docs: update locale detection API version

--- language_detection.go
+++ language_detection.go
@@ -9,18 +9,18 @@ import (
 	"net/url"
 )
 
-// Locales - https://wit.ai/docs/http/20170307#get__language_link
+// Locales - https://wit.ai/docs/http/20200513#get__language_link
 type Locales struct {
 	DetectedLocales []Locale `json:"detected_locales"`
 }
 
-// Locale - https://wit.ai/docs/http/20170307#get__language_link
+// Locale - https://wit.ai/docs/http/20200513#get__language_link
 type Locale struct {
 	Locale     string  `json:"locale"`
 	Confidence float64 `json:"confidence"`
 }
 
-// Detect - returns the detected languages from query - https://wit.ai/docs/http/20170307#get__language_link
+// Detect - returns the detected languages from query - https://wit.ai/docs/http/20200513#get__language_link
 func (c *Client) Detect(text string) (*Locales, error) {
 	resp, err := c.request(http.MethodGet, fmt.Sprintf("/language?q=%s", url.PathEscape(text)), "application/json", nil)
 	if err != nil {

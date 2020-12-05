572a982ac6fa5a2a0a104eff77727979d870c525
Nicky Logan <laurentiusdominick@yahoo.com>
2020-12-05 14:18:39 +0700

    feat: update app-related API to current version
    The new API version changes the request/response schema, plus added a
    way to version apps:
    - App struct changed to reflect the update
    - Added GetAppTags, GetAppTag, CreateAppTag, UpdateAppTag, MoveAppTag,
    and DeleteAppTag for tag-related APIs

--- apps.go
+++ apps.go
@@ -24,25 +24,22 @@ const (
 	Ongoing AppTrainingStatus = "ongoing"
 )
 
-// App - https://wit.ai/docs/http/20170307#get__apps_link
+// App - https://wit.ai/docs/http/20200513/#get__apps_link
 type App struct {
+	ID      string `json:"id,omitempty"`
 	Name    string `json:"name"`
 	Lang    string `json:"lang"`
 	Private bool   `json:"private"`
-	// Description presents when we get an app
-	Description string `json:"description,omitempty"`
-	// Use Desc when create an app
-	Desc string `json:"desc,omitempty"`
-	// ID presents when we get an app
-	ID string `json:"id,omitempty"`
-	// AppID presents when we create an app
-	AppID     string `json:"app_id,omitempty"`
-	CreatedAt string `json:"created_at,omitempty"`
-	Timezone  string `json:"timezone,omitempty"`
-	// Training information
-	LastTrainingDurationSecs int               `json:"last_training_duration_secs,omitempty"`
+
+	// Timezone is only used when creating/updating an app; it's
+	// not available when getting the details of an app.
+	Timezone string `json:"timezone,omitempty"`
+
+	CreatedAt Time `json:"created_at,omitempty"`
+
 	WillTrainAt              Time              `json:"will_train_at,omitempty"`
 	LastTrainedAt            Time              `json:"last_trained_at,omitempty"`
+	LastTrainingDurationSecs int               `json:"last_training_duration_secs,omitempty"`
 	TrainingStatus           AppTrainingStatus `json:"training_status,omitempty"`
 }
 
@@ -63,13 +60,15 @@ func (witTime *Time) UnmarshalJSON(input []byte) error {
 	return nil
 }
 
-// CreatedApp - https://wit.ai/docs/http/20170307#post__apps_link
+// CreatedApp - https://wit.ai/docs/http/20200513/#post__apps_link
 type CreatedApp struct {
 	AccessToken string `json:"access_token"`
 	AppID       string `json:"app_id"`
 }
 
-// GetApps - Returns an array of all apps that you own. https://wit.ai/docs/http/20170307#get__apps_link
+// GetApps - Returns an array of all apps that you own.
+//
+// https://wit.ai/docs/http/20200513/#get__apps_link
 func (c *Client) GetApps(limit int, offset int) ([]App, error) {
 	if limit <= 0 {
 		limit = 0
@@ -91,7 +90,9 @@ func (c *Client) GetApps(limit int, offset int) ([]App, error) {
 	return apps, err
 }
 
-// GetApp - returns map by ID. https://wit.ai/docs/http/20170307#get__apps__app_id_link
+// GetApp - Returns an object representation of the specified app.
+//
+// https://wit.ai/docs/http/20200513/#get__apps__app_link
 func (c *Client) GetApp(id string) (*App, error) {
 	resp, err := c.request(http.MethodGet, fmt.Sprintf("/apps/%s", url.PathEscape(id)), "application/json", nil)
 	if err != nil {
@@ -109,17 +110,9 @@ func (c *Client) GetApp(id string) (*App, error) {
 	return app, nil
 }
 
-// DeleteApp - deletes app by ID. https://wit.ai/docs/http/20170307#delete__apps__app_id_link
-func (c *Client) DeleteApp(id string) error {
-	resp, err := c.request(http.MethodDelete, fmt.Sprintf("/apps/%s", url.PathEscape(id)), "application/json", nil)
-	if err == nil {
-		resp.Close()
-	}
-
-	return err
-}
-
-// CreateApp - creates new app. https://wit.ai/docs/http/20170307#post__apps_link
+// CreateApp - creates new app.
+//
+// https://wit.ai/docs/http/20200513/#post__apps_link
 func (c *Client) CreateApp(app App) (*CreatedApp, error) {
 	appJSON, err := json.Marshal(app)
 	if err != nil {
@@ -140,23 +133,199 @@ func (c *Client) CreateApp(app App) (*CreatedApp, error) {
 	return createdApp, err
 }
 
-// UpdateApp - Updates an app. https://wit.ai/docs/http/20170307#put__apps__app_id_link
-func (c *Client) UpdateApp(id string, app App) (*App, error) {
+// UpdateApp - Updates an app.
+//
+// https://wit.ai/docs/http/20200513/#put__apps__app_link
+func (c *Client) UpdateApp(id string, app App) error {
 	appJSON, err := json.Marshal(app)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	resp, err := c.request(http.MethodPut, fmt.Sprintf("/apps/%s", url.PathEscape(id)), "application/json", bytes.NewBuffer(appJSON))
+	if err == nil {
+		resp.Close()
+	}
+
+	return err
+}
+
+// DeleteApp - deletes app by ID.
+//
+// https://wit.ai/docs/http/20200513/#delete__apps__app_link
+func (c *Client) DeleteApp(id string) error {
+	resp, err := c.request(http.MethodDelete, fmt.Sprintf("/apps/%s", url.PathEscape(id)), "application/json", nil)
+	if err == nil {
+		resp.Close()
+	}
+
+	return err
+}
+
+// AppTag - https://wit.ai/docs/http/20200513/#get__apps__app_tags__tag_link
+type AppTag struct {
+	Name string `json:"name,omitempty"`
+	Desc string `json:"desc,omitempty"`
+
+	CreatedAt Time `json:"created_at,omitempty"`
+	UpdatedAt Time `json:"updated_at,omitempty"`
+}
+
+// GetAppTags - Returns an array of all tag groups for an app.
+// Within a group, all tags point to the same app state (as a result of moving tags).
+//
+// https://wit.ai/docs/http/20200513/#get__apps__app_tags_link
+func (c *Client) GetAppTags(appID string) ([][]AppTag, error) {
+	resp, err := c.request(http.MethodGet, fmt.Sprintf("/apps/%s/tags", url.PathEscape(appID)), "application/json", nil)
 	if err != nil {
 		return nil, err
 	}
 
 	defer resp.Close()
 
-	var updatedApp *App
+	var tags [][]AppTag
 	decoder := json.NewDecoder(resp)
-	err = decoder.Decode(&updatedApp)
+	err = decoder.Decode(&tags)
+	return tags, err
+}
 
-	return updatedApp, err
+// GetAppTag - returns the detail of the specified tag.
+//
+// https://wit.ai/docs/http/20200513/#get__apps__app_tags__tag_link
+func (c *Client) GetAppTag(appID, tagID string) (*AppTag, error) {
+	resp, err := c.request(http.MethodGet, fmt.Sprintf("/apps/%s/tags/%s", url.PathEscape(appID), url.PathEscape(tagID)), "application/json", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Close()
+
+	var tag *AppTag
+	decoder := json.NewDecoder(resp)
+	err = decoder.Decode(&tag)
+	return tag, err
+}
+
+// CreateAppTag - Take a snapshot of the current app state, save it as a tag (version)
+// of the app. The name of the tag created will be returned in the response.
+//
+// https://wit.ai/docs/http/20200513/#post__apps__app_tags_link
+func (c *Client) CreateAppTag(appID string, tag string) (*AppTag, error) {
+	type appTag struct {
+		Tag string `json:"tag"`
+	}
+
+	tagJSON, err := json.Marshal(appTag{Tag: tag})
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.request(http.MethodPost, fmt.Sprintf("/apps/%s/tags", url.PathEscape(tag)), "application/json", bytes.NewBuffer(tagJSON))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Close()
+
+	// theresponse format is different than the one in get API.
+	var tmp appTag
+	decoder := json.NewDecoder(resp)
+	if err := decoder.Decode(&tmp); err != nil {
+		return nil, err
+	}
+
+	return &AppTag{Name: tmp.Tag}, nil
+}
+
+// UpdateAppTagRequest - https://wit.ai/docs/http/20200513/#put__apps__app_tags__tag_link
+type UpdateAppTagRequest struct {
+	Tag    string `json:"tag,omitempty"`
+	Desc   string `json:"desc,omitempty"`
+	MoveTo string `json:"move_to,omitempty"`
+}
+
+// UpdateAppTagResponse - https://wit.ai/docs/http/20200513/#put__apps__app_tags__tag_link
+type UpdateAppTagResponse struct {
+	Tag     string `json:"tag,omitempty"`
+	Desc    string `json:"desc,omitempty"`
+	MovedTo string `json:"moved_to,omitempty"`
+}
+
+// UpdateAppTag - Update the tag's name or description
+//
+// https://wit.ai/docs/http/20200513/#put__apps__app_tags__tag_link
+func (c *Client) UpdateAppTag(appID, tagID string, updated AppTag) (*AppTag, error) {
+	type tag struct {
+		Tag  string `json:"tag,omitempty"`
+		Desc string `json:"desc,omitempty"`
+	}
+
+	updateJSON, err := json.Marshal(tag{Tag: updated.Name, Desc: updated.Desc})
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.request(http.MethodPut, fmt.Sprintf("/apps/%s/tags/%s", url.PathEscape(appID), url.PathEscape(tagID)), "application/json", bytes.NewBuffer(updateJSON))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Close()
+
+	var tagResp tag
+	decoder := json.NewDecoder(resp)
+	err = decoder.Decode(&tagResp)
+	return &AppTag{Name: tagResp.Tag, Desc: tagResp.Desc}, err
+}
+
+type MovedAppTag struct {
+	Tag     string `json:"tag"`
+	Desc    string `json:"desc"`
+	MovedTo string `json:"moved_to"`
+}
+
+// MoveAppTag - move the tag to point to another tag.
+//
+// https://wit.ai/docs/http/20200513/#put__apps__app_tags__tag_link
+func (c *Client) MoveAppTag(appID, tagID string, to string, updated *AppTag) (*MovedAppTag, error) {
+	type tag struct {
+		Tag    string `json:"tag,omitempty"`
+		Desc   string `json:"desc,omitempty"`
+		MoveTo string `json:"move_to,omitempty"`
+	}
+
+	updateReq := tag{MoveTo: to}
+	if updated != nil {
+		updateReq.Tag = updated.Name
+		updateReq.Desc = updated.Desc
+	}
+
+	updateJSON, err := json.Marshal(updateReq)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.request(http.MethodPut, fmt.Sprintf("/apps/%s/tags/%s", url.PathEscape(appID), url.PathEscape(tagID)), "application/json", bytes.NewBuffer(updateJSON))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Close()
+
+	var tagResp *MovedAppTag
+	decoder := json.NewDecoder(resp)
+	err = decoder.Decode(&tagResp)
+	return tagResp, err
+}
+
+// DeleteAppTag - Permanently delete the tag.
+//
+// https://wit.ai/docs/http/20200513/#delete__apps__app_tags__tag_link
+func (c *Client) DeleteAppTag(appID, tagID string) error {
+	resp, err := c.request(http.MethodDelete, fmt.Sprintf("/apps/%s/tags/%s", url.PathEscape(appID), url.PathEscape(tagID)), "application/json", nil)
+	if err == nil {
+		resp.Close()
+	}
+
+	return err
 }
--- apps_test.go
+++ apps_test.go
@@ -5,34 +5,80 @@ package witai
 import (
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 )
 
 func TestGetApps(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte(`[{"name": "app1"}, {"name":"app2"}]`))
+		res.Write([]byte(`[
+			{
+				"id": "9890809890",
+				"name": "My_Second_App",
+				"lang": "en",
+				"private": false,
+				"created_at": "2018-01-01T00:00:01Z"
+			},
+			{
+				"id": "9890809891",
+				"name": "My_Third_App",
+				"lang": "en",
+				"private": false,
+				"created_at": "2018-01-02T00:00:01Z"
+			}
+		]`))
 	}))
 	defer func() { testServer.Close() }()
 
 	c := NewClient(unitTestToken)
 	c.APIBase = testServer.URL
-	apps, _ := c.GetApps(1, 0)
+	apps, err := c.GetApps(2, 0)
 
-	if len(apps) != 2 {
-		t.Fatalf("expected 2 apps, got: %v", apps)
+	wantApps := []App{
+		{
+			ID:        "9890809890",
+			Name:      "My_Second_App",
+			Lang:      "en",
+			Private:   false,
+			CreatedAt: Time{time.Date(2018, 1, 1, 0, 0, 1, 0, time.UTC)},
+		},
+		{
+			ID:        "9890809891",
+			Name:      "My_Third_App",
+			Lang:      "en",
+			Private:   false,
+			CreatedAt: Time{time.Date(2018, 1, 2, 0, 0, 1, 0, time.UTC)},
+		},
+	}
+
+	if err != nil {
+		t.Fatalf("nil error expected, got %v", err)
+	}
+	if !reflect.DeepEqual(wantApps, apps) {
+		t.Fatalf("expected\n\tapps: %v\n\tgot: %v", wantApps, apps)
 	}
 }
 
 func TestGetApp(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte(`{"name": "alarm-clock","training_status":"done","will_train_at":"2018-07-29T18:17:34-0700","last_training_duration_secs":42,"last_trained_at":"2018-07-29T18:16:34-0700"}`))
+		res.Write([]byte(`{
+			"id": "2802177596527671",
+			"name": "alarm-clock",
+			"lang": "en",
+			"private": false,
+			"created_at": "2018-07-29T18:15:34-0700",
+			"last_training_duration_secs": 42,
+			"will_train_at": "2018-07-29T18:17:34-0700",
+			"last_trained_at": "2018-07-29T18:18:34-0700",
+			"training_status": "done"
+		}`))
 	}))
 	defer func() { testServer.Close() }()
 
 	c := NewClient(unitTestToken)
 	c.APIBase = testServer.URL
-	app, err := c.GetApp("my-id")
+	app, err := c.GetApp("2802177596527671")
 	if err != nil {
 		t.Fatalf("not expected err, got: %s", err.Error())
 	}
@@ -53,7 +99,7 @@ func TestGetApp(t *testing.T) {
 		t.Fatalf("Expected 42 got %v", app.LastTrainingDurationSecs)
 	}
 
-	expectedLastTrainTime, _ := time.Parse(WitTimeFormat, "2018-07-29T18:16:34-0700")
+	expectedLastTrainTime, _ := time.Parse(WitTimeFormat, "2018-07-29T18:18:34-0700")
 	if !app.LastTrainedAt.Time.Equal(expectedLastTrainTime) {
 		t.Fatalf("expected %v got: %v", app.WillTrainAt, expectedLastTrainTime)
 	}
@@ -80,7 +126,7 @@ func TestCreateApp(t *testing.T) {
 
 func TestDeleteApp(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte(`{}`))
+		res.Write([]byte(`{"success": true}`))
 	}))
 	defer func() { testServer.Close() }()
 
@@ -93,20 +139,208 @@ func TestDeleteApp(t *testing.T) {
 
 func TestUpdateApp(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte(`{"description": "updated"}`))
+		res.Write([]byte(`{"success": true}`))
 	}))
 	defer func() { testServer.Close() }()
 
 	c := NewClient(unitTestToken)
 	c.APIBase = testServer.URL
 
-	app, err := c.UpdateApp("appid", App{
-		Description: "new desc",
+	err := c.UpdateApp("appid", App{
+		Lang:     "fr",
+		Timezone: "Europe/Paris",
 	})
 	if err != nil {
 		t.Fatalf("err=nil expected, got: %v", err)
 	}
-	if app.Description != "updated" {
-		t.Fatalf("description=updated expected, got: %s", app.Lang)
+}
+
+func TestGetAppTags(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`[
+			[
+				{
+					"name": "v3",
+					"created_at": "2019-09-14T20:29:53-0700",
+					"updated_at": "2019-09-14T20:29:53-0700",
+					"desc": "third version"
+				},
+				{
+					"name": "v2",
+					"created_at": "2019-08-08T11:05:35-0700",
+					"updated_at": "2019-08-08T11:09:17-0700",
+					"desc": "second version, moved to v3"
+				}
+			],
+			[
+				{
+					"name": "v1",
+					"created_at": "2019-08-08T11:02:52-0700",
+					"updated_at": "2019-09-14T15:45:22-0700",
+					"desc": "legacy first version"
+				}
+			]
+		]`))
+	}))
+	defer testServer.Close()
+
+	c := NewClient(unitTestToken)
+	c.APIBase = testServer.URL
+
+	wantTags := [][]AppTag{
+		{
+			{Name: "v3", Desc: "third version"},
+			{Name: "v2", Desc: "second version, moved to v3"},
+		},
+		{
+			{Name: "v1", Desc: "legacy first version"},
+		},
+	}
+
+	tags, err := c.GetAppTags("appid")
+	if err != nil {
+		t.Fatalf("expected nil err, got: %v", err)
+	}
+
+	if len(tags) != len(wantTags) {
+		t.Fatalf("expected\n\ttags: %v\n\tgot: %v", wantTags, tags)
+	}
+
+	for i, group := range tags {
+		wantGroup := wantTags[i]
+
+		if len(group) != len(wantGroup) {
+			t.Fatalf("expected\n\ttags[%v]: %v\n\tgot: %v", i, wantTags, tags)
+		}
+
+		for j, tag := range group {
+			wantTag := wantGroup[j]
+			if tag.Name != wantTag.Name {
+				t.Fatalf("expected\n\ttags[%v][%v].Name: %v\n\tgot: %v", i, j, wantTag.Name, tag.Name)
+			}
+			if tag.Desc != wantTag.Desc {
+				t.Fatalf("expected\n\ttags[%v][%v].Desc: %v\n\tgot: %v", i, j, wantTag.Desc, tag.Desc)
+			}
+		}
+	}
+}
+
+func TestGetAppTag(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{
+			"name": "v1",
+			"created_at": "2019-08-08T11:02:52-0700",
+			"updated_at": "2019-09-14T15:45:22-0700",
+			"desc": "first version"
+		}`))
+	}))
+	defer testServer.Close()
+
+	c := NewClient(unitTestToken)
+	c.APIBase = testServer.URL
+
+	tag, err := c.GetAppTag("appid", "tagid")
+	if err != nil {
+		t.Fatalf("expected nil err, got: %v", err)
+	}
+
+	wantTag := &AppTag{
+		Name: "v1",
+		Desc: "first version",
+	}
+
+	if tag.Name != wantTag.Name {
+		t.Fatalf("expected\n\ttag.Name: %v, got: %v", wantTag.Name, tag.Name)
+	}
+	if tag.Desc != wantTag.Desc {
+		t.Fatalf("expected\n\ttag.Desc: %v, got: %v", wantTag.Desc, tag.Desc)
+	}
+}
+
+func TestCreateAppTag(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"tag": "v_1"}`))
+	}))
+	defer testServer.Close()
+
+	c := NewClient(unitTestToken)
+	c.APIBase = testServer.URL
+
+	tag, err := c.CreateAppTag("appid", "v$1")
+	if err != nil {
+		t.Fatalf("expected nil err, got: %v", err)
+	}
+
+	wantTag := &AppTag{Name: "v_1"}
+	if tag.Name != wantTag.Name {
+		t.Fatalf("expected\n\ttag.Name: %v, got: %v", wantTag.Name, tag.Name)
+	}
+}
+
+func TestUpdateAppTag(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{
+			"tag": "v1.0",
+			"desc": "new description"
+		}`))
+	}))
+	defer testServer.Close()
+
+	c := NewClient(unitTestToken)
+	c.APIBase = testServer.URL
+
+	updated, err := c.UpdateAppTag("appid", "v1", AppTag{Name: "v1.0", Desc: "new description"})
+	if err != nil {
+		t.Fatalf("expected nil err, got: %v", err)
+	}
+
+	wantUpdated := &AppTag{Name: "v1.0", Desc: "new description"}
+
+	if !reflect.DeepEqual(updated, wantUpdated) {
+		t.Fatalf("expected\n\tupdated: %v\n\tgot: %v", wantUpdated, updated)
+	}
+}
+
+func TestMoveAppTag(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{
+			"tag": "v1.0",
+			"desc": "1.0 version, moved to 1.1 version",
+			"moved_to": "v1.1"
+		}`))
+	}))
+	defer testServer.Close()
+
+	c := NewClient(unitTestToken)
+	c.APIBase = testServer.URL
+
+	moved, err := c.MoveAppTag("appid", "v1", "v1.1", &AppTag{Name: "v1.0", Desc: "1.0 version, moved to 1.1 version"})
+	if err != nil {
+		t.Fatalf("expected nil err, got: %v", err)
+	}
+
+	wantMoved := &MovedAppTag{
+		Tag:     "v1.0",
+		Desc:    "1.0 version, moved to 1.1 version",
+		MovedTo: "v1.1",
+	}
+
+	if !reflect.DeepEqual(moved, wantMoved) {
+		t.Fatalf("expected\n\tmoved: %v\n\tgot: %v", wantMoved, moved)
+	}
+}
+
+func TestDeleteAppTag(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"success": true}`))
+	}))
+	defer testServer.Close()
+
+	c := NewClient(unitTestToken)
+	c.APIBase = testServer.URL
+
+	err := c.DeleteAppTag("appid", "tagid")
+	if err != nil {
+		t.Fatalf("expected nil err, got: %v", err)
 	}
 }
